df6176a519b197519d3ba999ff6184561e774cdc
Nathan Xander Lada <nlada27@gmail.com>
2026-04-04 23:37:46 +0800

    Fixed ghost in the machine

--- Backend/src/main/java/com/payslip/payroll/service/EmailService.java
+++ Backend/src/main/java/com/payslip/payroll/service/EmailService.java
@@ -14,7 +14,7 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    @Value("${spring.mail.username}")
+    @Value("${app.mail.from}")
     private String officeEmail;
 
     public EmailService(JavaMailSender mailSender) {
